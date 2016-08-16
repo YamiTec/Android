78e8625ccd75f97d1a3cdc8d4d650face05e3040
Ephyllus Moura <ephyllus2@gmail.com>
2016-08-16 09:37:42 -0300

    Update BuildConfig.java

--- gen/com/survivingwithandroid/facedetection/BuildConfig.java
+++ gen/com/survivingwithandroid/facedetection/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.survivingwithandroid.facedetection;
+package com.yamitec.facedetection;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
